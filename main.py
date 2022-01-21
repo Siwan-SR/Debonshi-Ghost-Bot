c5c3a5898fb710cb1793ea8c8beb01ddf7d58580
Siwan SR <97611483+Siwan-SR@users.noreply.github.com>
2022-01-21 08:51:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# Copyrights Siwan SR 2022. When forking this file please give credits to @SiwanSR   (github) or @TetCode (in Replit). Read copyrights.txt
+
+
+
+# Making a character
+
+ghost = 'Debonshi'
+
+print("Hello, I am", ghost)
+print("I am a ghost! A friendly ghost")
+
+#Scene 1
+
+# Asking Name
+
+name = input ("What's your name human?\n")
+print("That's a nice name",name + "!\n")
+
+# Asking age
+
+age = input ("What's your age?\n")
+print("Young Indeed", name + "." )
+
+# Asking if user is scared of the ghost
+
+question1 = input ("Are you scared of me? I am very scary: 1. I am scared. 2. No I am very brave\n")
+
+if question1 == "1":
+  print("Don't worry, I am a friendly ghost! I won't haunt you",name + ".\n")
+
+elif question1 == "2":
+  print("That's very brave of a", age + " year old. Always stay brave!\n")
+
+# Asking usual questions
+
+favfood = input ("What's your favorite food?\n")
+print("Oh my favorite food is also",favfood + " and beef chops\n")
+
+hobbies = input ("Don't mind me asking but what's your hobby?\n")
+print("Interesting... My hobby is to haunt bad and rude people.\n")
+
+
+# Asking if user wants to scare someone.
+
+
+hauntwish = input ("Do you want me to scare any of your friends,\ntell me their name and I will haunt them until they become good people!\n")
+print("Okay, I will start haunting",hauntwish + " at midnight!\n")
+
+
+# Scene 2
+
