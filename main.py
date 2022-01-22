a918e899f50a7a140bb82ad139299ea9e0029c0a
Siwan SR <97611483+Siwan-SR@users.noreply.github.com>
2022-01-22 21:37:19 +0600

    Initial commit

--- main.py
+++ main.py
@@ -18,18 +18,12 @@ print("I am a ghost! A friendly ghost")
 
 # Asking Name
 
-name = input 
-
-while(not(name == "")):
-    name = input("What is your name human?\n")
-    if not re.match("^[a-z]*$", name):
-        print("Only letters are allowed")
-        sys.exit()
-    elif len(name) == 0:
-        print("Insufficient characters.")
-        sys.exit()
-    else:
-        name.append(name)
+name = input("What's your name human?\n")
+if name.isalpha() is False:
+ print("Only letters are allowed!")
+ sys.exit()
+else:
+ print("Nice name "+name)
 
 # Asking age
 
@@ -47,7 +41,7 @@ if question1 == "1":
   print("Don't worry, I am a friendly ghost! I won't haunt you",name + ".\n")
 
 elif question1 == "2":
-  print("That's very brave of a", age + " year old. Always stay brave!\n")
+  print("That's very brave of you",name + " year old. Always stay brave!\n")
 
 else:
   sys.exit()
@@ -55,57 +49,45 @@ else:
 
 # Asking usual questions
 
-favfood = input
+favfood = input("What's your favorite food?\n")
+if favfood.isalpha() is False:
+ print("Only letters are allowed!")
+ sys.exit()
+else:
+ print("My favorite food is also", favfood + " and beef chops.")
 
-while(not(favfood == "")):
-    favfood = input("What's your favorite food?\n")
-    if not re.match("^[a-z]*$", favfood):
-        print("Only letters are allowed")
-        sys.exit()
-    elif len(favfood) == 0:
-        print("Insufficient characters.")
-        sys.exit()
-    else:
-        favfood.append(favfood)
 
-hobbies = input 
+hobby = input("What's your hobby?\n")
+if name.isalpha() is False:
+ print("Only letters are allowed!")
+ sys.exit()
+else:
+ print("Interesting... my hobby is to haunt bad people.")
 
-while(not(hobbies == "")):
-    hobbies = input("What's your favorite food?\n")
-    if not re.match("^[a-z]*$", hobbies):
-        print("Only letters are allowed")
-        sys.exit()
-    elif len(hobbies) == 0:
-        print("Insufficient characters.")
-        sys.exit()
-    else:
-        hobbies.append(hobbies)
 
 
 # Asking if user wants to scare someone.
 
 
-wish1 = input ("Do you want me to scare any of your friends,\ntell me their name and I will haunt them until they become good people! 1. Yes. 2.No\n")
+wish1 = input ("Do you want me to scare any of your friends,\ntell me their name and I will haunt them until they become good people!")
 
-if wish1 == "1":
 
-  haunt1 = input ("Who is the first person you want me to haunt?\n")
-  haunt2 = input ("Who is the second person you want me to haunt?\n")
-  haunt3 = input ("Who is the third person you want me to haunt?\n")
+haunt1 = input ("Who is the first person you want me to haunt?\n")
+haunt2 = input ("Who is the second person you want me to haunt?\n")
+haunt3 = input ("Who is the third person you want me to haunt?\n")
   
-  print("Okay, I will start haunting",haunt1 + ",", haunt2 + " and", haunt3 + " at midnight!\n")
-  hauntlist = haunt1, haunt2, haunt3
+print("Okay, I will start haunting",haunt1 + ",", haunt2 + " and", haunt3 + " at midnight!\n")
 
-elif wish1 == "2":
-  print("Okay I won't scare anyone!")
 
 
 
 
 # Scene 2
 
-print(system + ": The next day at 9 pm.\n")
+print(system + ": The next day at 9 am.\n")
 
-print(user + ": I wonder if",ghost + " scared", hauntlist)
+print(user + ": I wonder if",ghost + " scared them")
 
-print("Hello",name + ", last night I scared your friends, like a lot. Eh.. might have broken a few things, but let's not get into it here.")
+print(ghost + ": Hello",name + ", last night I scared your friends, like a lot. Eh.. might have broken a few things, but let's not get into it here.")
+
+print(system + ": Ghost disappears")
